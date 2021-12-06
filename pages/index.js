4a3e38062412ac59f78acded51b3f05e4e2db4db
Abdelhakim Jebabra <59029617+HAKIM2000-JE@users.noreply.github.com>
2021-12-06 16:40:12 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -29,23 +29,10 @@ export default function Home() {
 <script
   src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
   crossOrigin></script>
+
   <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=320, height=device-height, target-densitydpi=medium-dpi" />
       
-      <script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '3215114661844754');
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=3215114661844754&ev=PageView&noscript=1"
-/></noscript>
+     
       
       </Head>
       
